fea9d503d674dfe98d23af18f9a0fc72fe0a000d
aradoubob <aradoubob@live.fr>
2025-05-29 10:51:51 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,22 +39,24 @@ export default function Navbar() {
                       <PhoneIcon className="h-4 w-4 text-goldenrod mr-2" />
                       <a href="tel:0786048044" className="text-lg text-goldenrod hover:text-yellow-500">0786 322 385</a>
                     </div>
-                  </div>
-                  <div className="flex items-center space-x-4">
-                  <a href="https://www.facebook.com/profile.php?id=61557345880478" rel="noopener noreferrer">
-                    <Image src="/images/face.jpg" alt="Facebook" className="h-8 w-8" />
-                    target="_blank" 
-                   
-                  </a>
-                  <a
-  href="https://www.instagram.com/claireimmobiliare"
-  target="_blank"
-  rel="noopener noreferrer"
- 
->
-  <Image src="/images/insta.jpg" alt="Instagram" className="h-8 w-8" />
-</a>
-                </div>
+                    
+                 </div>
+<div className="flex items-center space-x-4">
+  <a
+    href="https://www.facebook.com/profile.php?id=61557345880478"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <Image src="/images/face.jpg" alt="Facebook" className="h-8 w-8" />
+  </a>
+  <a
+    href="https://www.instagram.com/claireimmobiliare"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <Image src="/images/insta.jpg" alt="Instagram" className="h-8 w-8" />
+  </a>
+</div>
                     </div>
               </div>
 
