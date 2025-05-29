cf5fae0b607087ea8a12d74d5d01ad0028044372
aradoubob <aradoubob@live.fr>
2025-05-29 10:59:38 +0300

    Update Partners.jsx

--- src/pages/Partners.jsx
+++ src/pages/Partners.jsx
@@ -29,6 +29,55 @@ export default function Partners() {
               Vizitează website-ul 
             </a>
           </div>
+
+             <div className="bg-black rounded-lg shadow-lg p-6 mb-6 border border-goldenrod">
+          <div className="flex items-center mb-4">
+              <img 
+                src="/images/logoaxe.jpg" 
+                alt="Logo AXE MOBILIER" 
+                className="h-20 w-auto mr-4"
+              />
+               <div className="flex flex-col">
+                <h2 className="text-2xl font-bold text-goldenrod">AXE MOBILIER - Tâmplarie</h2>             
+                
+                 href="https://axe-m.com/" 
+
+                <p className="text-goldenrod">Partener de încredere pentru lucrări de tâmplărie</p>
+              </div></div>
+            <a
+      
+              href="https://axe-m.com/" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="text-goldenrod hover:text-white transition-colors">
+              Vizitează website-ul 
+            </a>
+          </div>
+
+             <div className="bg-black rounded-lg shadow-lg p-6 mb-6 border border-goldenrod">
+          <div className="flex items-center mb-4">
+              <img 
+                src="/images/logoaxe.jpg" 
+                alt="Logo AXE MOBILIER" 
+                className="h-20 w-auto mr-4"
+              />
+               <div className="flex flex-col">
+                <h2 className="text-2xl font-bold text-goldenrod">AXE MOBILIER - Tâmplarie</h2>             
+                
+                 href="https://axe-m.com/" 
+
+                <p className="text-goldenrod">Partener de încredere pentru lucrări de tâmplărie</p>
+              </div></div>
+            <a
+      
+              href="https://axe-m.com/" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="text-goldenrod hover:text-white transition-colors">
+              Vizitează website-ul 
+            </a>
+          </div>
+          
         </div>
       </div>
     </div>
